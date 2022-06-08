df165e3691c87b1a41b3c3a65010586d94cc776f
Hafsa Dar <hafsadar1998@gmail.com>
2022-06-09 01:50:09 +0500

    abc

--- app.js
+++ app.js
@@ -68,15 +68,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-// var allowCrossDomain = function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-// };
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
 
-// app.configure(function () {
-//   app.use(allowCrossDomain);
-// });
+app.use(allowCrossDomain);
 
 app.use(cookieParser());
 app.use(express.json());
