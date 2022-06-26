933b04c9f90f3e56ab9d9c25368e1c5d8473da58
Hafsa Dar <hafsadar1998@gmail.com>
2022-06-26 22:32:46 +0500

    member changes

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -4,10 +4,4 @@ const memberController = require("../controllers/memberController");
 
 router.get("/members", memberController.members);
 
-// router.get("/silverMember", memberController.silverMember);
-
-// router.get("/bronzeMember", memberController.bronzeMember);
-
-//router.get("/showStatus", memberController.showMemberStatus);
-
 module.exports = router;
